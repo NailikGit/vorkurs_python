9b29ec7bf01e584a4724cd46ab09825d1bb0a754
NailikGit <kilian.smeets@web.de>
2024-09-18 18:40:58 +0200

    finished wikipedia game script

---
+++ 500pts/wikipedia_game.py
@@ -0,0 +1,31 @@
+import requests
+
+if __name__ == "__main__":
+    begin: str = "Frühstücksfleisch"
+    end: str = "Python (Programmiersprache)"
+    req: str = begin
+
+    path: list[str] = [begin]
+
+    while not req == end:
+        r = requests.get(f"https://de.wikipedia.org/w/api.php?action=query&prop=links&pllimit=max&format=json&titles={req}")
+        d: dict = r.json()
+        l: list[dict[str, str]] = d["query"]["pages"][list(d["query"]["pages"].keys())[0]]["links"]
+
+        counter: int = 0
+        for a in l:
+            print(f"{counter}: {a["title"]}")
+            counter += 1
+
+        user: int = int(input("continue to index: "))
+
+        req: str = l[user]["title"]
+
+        path.append(req)
+
+    print("path taken: ", end = "")
+    for i in path:
+        if i == begin:
+            print(i, end = "")
+        else:
+            print(f", {i}", end = "")
